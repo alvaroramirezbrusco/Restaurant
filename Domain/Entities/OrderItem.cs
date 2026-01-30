bdd5290661480b8b8792ef390600f057d8f0b35e
alvaroramirezbrusco <alvaroramirezbrusco@gmail.com>
2026-01-30 11:58:18 -0300

    Agregar archivos de proyecto.

---
+++ Domain/Entities/OrderItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class OrderItem
+    {
+        public long OrderItemId { get; set; }
+        public Order OrderNavigator { get; set; }
+        public long Order { get; set; }
+        public Dish DishNavigator { get; set; }
+        public Guid Dish { get; set; }
+        public int Quantity { get; set; }
+        public string Notes { get; set; }
+        public Status StatusNavigator { get; set; }
+        public int Status { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
---
+++ Domain/Entities/Status.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Status
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Order> Orders { get; set; }
+        public IList<OrderItem> OrderItems { get; set; }
+    }
+}
